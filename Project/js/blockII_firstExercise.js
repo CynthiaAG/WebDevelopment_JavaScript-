f6441d15619ca99d47e33325e1ae4d2ef7f1a3a1
CynthiaAG <cynthia.cjag@gmail.com>
2020-09-29 20:10:55 +0100

    Block II finished.

---
+++ Project/js/blockII_firstExercise.js
@@ -0,0 +1,40 @@
+'use strict'
+
+function getElements (){
+	var arrayElement = new Array();
+	for (var i = 0; i <= 5; i++) {
+		arrayElement.push(prompt("Enter the elements", ""));
+	}
+	return arrayElement;
+}
+
+function showArray(elements, customText=""){
+	document.write("<h1> Content to array" + customText + "</h1>");
+	document.write("<ul>");
+	elements.forEach((element, index) => {
+		document.write("<li>"+ element + "</li>");
+	});
+	document.write("</ul>");
+}
+
+function searchElement(elements){
+	var element = parseInt(prompt("Enter the elements to search", ""));
+	var index= elements.findIndex(number => number == element);
+	if(index && index > -1){
+		document.write("<h1> Found at position: " + index + "</h1>");
+	}
+
+}
+
+var result = new Array();
+result = getElements();
+console.log(result);
+/*result.sort(function (a, b) { return a-b});
+result.sort();
+result.reverse();
+showArray(result, " organized");
+document.write(result.length); */
+searchElement(result);
+
+
+
